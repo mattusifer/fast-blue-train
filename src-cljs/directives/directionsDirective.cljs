d189495886c15d4a532e5899e4859b10d6474e13
Matt Usifer <mattusifer@gmail.com>
2016-03-10 08:24:31 -0500

    Integrated with google maps renderer

--- src-cljs/directives/directionsDirective.cljs
+++ src-cljs/directives/directionsDirective.cljs
@@ -19,19 +19,12 @@
    :controller 
    (fn [] 
      (declare *map*) 
+     (declare renderer)
      (declare start-input)
      (declare end-input)
      (declare places) 
 
      (def directions-service (js/google.maps.DirectionsService.))
-     
-     (defn- html-aggregator 
-       "create HTML table from directions"
-       [old next-step]
-       (str old
-            "<tr><td>" (.-instructions next-step) "</td>"
-            "<td>" (.-text (.-distance next-step)) "</td>"
-            "<td>" (.-text (.-duration next-step)) "</tr>"))
      
      (defn- on-place-changed 
        "re-position map based on the value of the start element"
@@ -41,25 +34,13 @@
            (.panTo *map* (.-location (.-geometry place)))
            (.setZoom *map* 15))))
      
-     (defn- show-steps
-       "create HTML table from directions"
-       [response]
-       (let [steps (.-steps response)]
-         (loop [cur-step (first steps)
-                other-steps (rest steps)
-                html "<tr><th>Instructions</th><th>Distance</th><th>Duration</th></tr>"]
-           (if (empty? other-steps)
-             (set! (.-innerHTML (sel1 :#instructions)) html)
-             (let [new-html (html-aggregator html cur-step)]
-               (recur (first other-steps) (rest other-steps) new-html))))))
-
      (defn- get-directions-promise 
        "wrapper function to get a promise from the (callback-driven) Google Maps API"
        [mode]
        (let [deferred (.defer $q)
              dir-service-handler (fn [res stat]
                                    (if (= stat js/google.maps.DirectionsStatus.OK)
-                                     (.resolve deferred (first (.-legs (first (.-routes res)))))
+                                     (.resolve deferred res)
                                      (.reject deferred (str "Failed due to " stat))))
              req (clj->js {"origin" (.-value (sel1 :#startLocationInput))
                            "destination" (.-value (sel1 :#endLocationInput))
@@ -75,16 +56,21 @@
        (let [modes [js/google.maps.TravelMode.DRIVING
                     js/google.maps.TravelMode.WALKING
                     js/google.maps.TravelMode.TRANSIT]
-             get-duration (fn [response] 
-                        (.-value (.-duration response)))
+             promises (map get-directions-promise modes)
+             get-duration (fn [res] 
+                        (.-value 
+                         (.-duration 
+                          (first (.-legs (first (.-routes res)))))))
              get-min (fn [values] 
-                       (show-steps (apply min-key get-duration values)))
-             promises (map get-directions-promise modes)]
+                       (let [min (apply min-key get-duration values)
+                             mode (.-travelMode (.-request min))]
+                         (.setDirections renderer min)))]
          (.then (.all $q (into-array promises)) get-min)))
      
      ; main
-     (let [map-opts (clj->js {"zoom" 8
-                              "center" (js/google.maps.LatLng. -34.397 150.644)
+     (let [philly (js/google.maps.LatLng. 39.95 -75.1667)
+           map-opts (clj->js {"zoom" 9
+                              "center" philly
                               "mapTypeId" "roadmap"})
            ac-opts (clj->js {"types" ["address"]
                              "componentRestrictions" {"country" "us"}})
@@ -92,8 +78,13 @@
            start-elem (sel1 :#startLocationInput)
            end-elem (sel1 :#endLocationInput)
            submit-elem (sel1 :#directionSubmit)]
+
+       ; configure map and renderer
+       (set! renderer (google.maps.DirectionsRenderer.))
        (set! *map* (js/google.maps.Map. map-elem map-opts))
-       
+       (.setMap renderer *map*)
+       (.setPanel renderer (sel1 :#instructions-container))
+
        ;Start Input
        (set! start-input (js/google.maps.places.Autocomplete. start-elem ac-opts))
        (set! places (js/google.maps.places.PlacesService. *map*))
